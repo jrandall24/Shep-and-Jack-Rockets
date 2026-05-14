19c82918509b4284893d776b9e4298d12e72da3e
jrandall24 <jrandall27@gcds.net>
2026-05-14 09:21:48 -0400

    Updation

--- rocketstuff.py
+++ rocketstuff.py
@@ -1,19 +1,26 @@
 import math
 
-def goalLine():
+def goalLine(): # g=11
     data = int(input('Give launch location: '))
     data_m = data*0.9144
     v = math.sqrt((data_m*11)/(math.sin(2*0.872665))) #launch angle is always 50
-    print(v)
     p = math.exp((v+45.18761)/15.77984)
     return p, 50
 
-def fieldGoal():
-    return 'none'
+def fieldGoal(): # 20 ft = 6.069 m
+    data = input('Give launch angle and launch location: ').split()
+    angle = int(data[0])*(math.pi/180)
+    x = (int(data[1])*0.9144)+9.144
+    try:
+        v = math.sqrt((11*(x**2))/((2*(math.cos(angle)**2))*((x*math.tan(angle))-6.069)))
+        p = math.exp((v+45.18761)/15.77984)
+        return p
+    except:
+        return 'unavailable'
 
 def main():
     while True:
-        action = input('''Action:
+        action = input('''Actions:
 1. Hit goal line
 2. Field goal
 3. End
@@ -21,7 +28,7 @@ Choose your action: ''')
         if action == '1':
             print(f'Pressure: {goalLine()[0]} psi Angle: 50 degrees')
         elif action == '2':
-            print(fieldGoal())
+            print(f'Pressure: {fieldGoal()} psi')
         elif action == '3':
             print('See ya')
             break
